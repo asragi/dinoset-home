9c4347eeaf4e747662113fa31ecdb74c07c49718
asRagi <ragi-justaway@hotmail.co.jp>
2023-07-17 22:11:18 +0900

    add scss config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
 }
 
 module.exports = nextConfig
