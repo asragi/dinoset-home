c2d972b2ecc2e4366905dce6bfa39bb0cc921878
Ragi <ragi-justaway@hotmail.co.jp>
2023-07-27 10:23:29 +0900

    Add news

--- src/components/middleContainer.tsx
+++ src/components/middleContainer.tsx
@@ -1,8 +1,10 @@
 import styles from './middleContainer.module.scss';
+import { OneLineNews } from './oneLineNews';
 import { ProductComponent } from './productComponent';
 
 export const MiddleContainer = () => (
     <div className={styles.container}>
+        <OneLineNews />
         <ProductComponent />
     </div>
 );
---
+++ src/components/oneLineNews.tsx
@@ -0,0 +1,9 @@
+import styles from "./oneLineNews.module.scss";
+
+export const OneLineNews = () => (
+  <div className={styles.container}>
+    <span className={styles.news}>News</span>
+    <span className={styles.date}>2023/07/22</span>
+    <span>『恐竜定食』公式Webサイトオープン！</span>
+  </div>
+);
