e37a9bc4765fe9d66067f35537310fa11bde19ce
Ragi <ragi-justaway@hotmail.co.jp>
2023-08-02 19:12:02 +0900

    Update news

--- src/components/oneLineNews.tsx
+++ src/components/oneLineNews.tsx
@@ -3,7 +3,7 @@ import styles from "./oneLineNews.module.scss";
 export const OneLineNews = () => (
   <div className={styles.container}>
     <span className={styles.news}>News</span>
-    <span className={styles.date}>2023/07/22</span>
+    <span className={styles.date}>2023/08/02</span>
     <span>『恐竜定食』公式Webサイトオープン！</span>
   </div>
 );
